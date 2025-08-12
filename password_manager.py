f125a89b965ee8f2acce71c8c86ce5ef6e43cf6a
kartik8077 <kartikbhardwaj0619@gmail.com>
2025-08-12 18:01:43 +0530

    this is just some progres

--- password_manager.py
+++ password_manager.py
@@ -1,1 +1,21 @@
 pwd=input("What is the master password ?")
+
+def view():
+    pass
+
+def add():
+    pass
+
+while True:
+    mode=input("Would to add new password or view existing ones (view/add),press q to quit ?").lower()
+    if mode=="q":
+        break
+    
+    if mode=="view":
+        pass
+    elif mode=="add":
+        pass
+    else:
+        print("invalid mode !")
+        continue
+
