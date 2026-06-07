aac97c7e16afb8bf321d5cb310fd89fb44a2bb7d
kartik8077 <kartikbhardwaj0619@gmail.com>
2026-06-07 09:01:04 +0530

    this is the us state guessing game

---
+++ stat_guessing_game/main.py
@@ -0,0 +1,42 @@
+import turtle
+import pandas
+
+
+screen = turtle.Screen()
+screen.title("U.S STATES GAME")
+image= "blank_states_img.gif"
+screen.addshape(image)
+turtle.shape(image)
+
+data = pandas.read_csv("50_states.csv")
+state_list = data.state.to_list()
+x_list = data.x.to_list()
+y_list = data.y.to_list()
+
+guessed_state = []
+
+while len(guessed_state) < 50:
+    answer_state = screen.textinput(title=f"Guessed state {len(guessed_state)} / 50 ", prompt="what state do you have?").title()
+
+    if answer_state == "Exit":
+        missing_states = []
+        for state in state_list:
+            if state not in guessed_state:
+                missing_states.append(state)
+        print(missing_states)
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("states_to_learn.csv")
+        break
+
+    if answer_state in state_list:
+        guessed_state.append(answer_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer_state]
+        t.goto(state_data.x.item(),state_data.y.item())
+        t.write(answer_state,font=("Arial",15,"bold"))
+
+
+
+# screen.exitonclick()
